8ec813b58d9c70b37e4984eb61f6bdb762ecabeb
piotrkosinski1992 <piotrkosinski1992@o2.pl>
2019-05-22 17:50:03 +0200

    First code review. Some changes needs to be done

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -1,13 +1,22 @@
 package main;
 
 
-public class Calculator {
+public class Calculator { //nazwałbym CalculatorServer - tutaj byłyby metody stricte związane z działaniami
+    // matematycznymi
 
 
     public float x;
     public float y;
 
 
+    //zrobiłbym tutaj konstruktor dla klasy calculator który przyjmowałby na starcie wartości x i y wtedy wtedy w
+    // main będziesz wywoływać Calculator calculator = new Calculator(x,y)
+
+
+
+    //stworzyłbym trzecią klasę o nazwie CalculatorView (odpowiedzialną za rzeczy związane z wyświetlananiem na
+    // konsole. Miałaby takie metody jak printMenu() - do wyswietlenia menu, pickMenuItem() - do wybierania operacji
+    // matematycznej getNumbersToCalculate() - do zebrania liczb do obliczeń
     public void Menu(){
         System.out.println("-----------------");
         System.out.println("------MENU-------");
@@ -39,15 +48,17 @@ public class Calculator {
 
     public float multiply() {
 
-        float z3 = x * y;
-
+        float z3 = x * y; // jak podaję liczbę w stylu sto biliardów razy biliard dostaję informację infinity zamiast
+        // wyniku
+        //czym to jest spowodowane? jesteśmy w stanie to jakoś wyłapać?
         return z3;
     }
 
 
     public float divide() {
 
-        float z4 = x / y;
+        // float z4 = x / y; tutaj nie ma sensu przypisywać wyniku do zmiennej bo od razu chcemy ją przekazać. możesz
+        // od razu zrobić: return x/y;
 
         return z4;
     }
